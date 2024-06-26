a911b4fd5ec9d8bc23955f7caeb800700ad2755a
liuuu <liugongmingg@163.com>
2024-06-26 21:22:46 +0800

    create a min heap to build a priority queue

---
+++ min_heap.h
@@ -0,0 +1,20 @@
+#ifndef MIN_HEAP_H_
+#define MIN_HEAP_H_
+
+#include "tree_node.h"
+#include <memory>
+#include <vector>
+
+class MinHeap {
+public:
+    void push(std::shared_ptr<TreeNode> node);
+    std::shared_ptr<TreeNode> pop();
+    int size();
+private:
+    int size_ = 0;
+    std::vector<std::shared_ptr<TreeNode>> heap_;
+    void minimize_heap(int i);
+    void maintain_heap(int i);
+};
+
+#endif

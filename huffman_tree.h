cd27359b753e3721392250a840a0e1f64a066caa
liuuu <liugongmingg@163.com>
2024-06-27 13:19:16 +0800

    add some annotations

--- huffman_tree.h
+++ huffman_tree.h
@@ -24,16 +24,37 @@ public:
     std::vector<char> to_byte_array(std::string &s);
 
     /**
-     * @brief Huffman Tree Constructor
-     * @param code :
+     * @brief Huffman Tree Constructor, build a Huffman tree from the given mapping from char to str
+     * @param code :a mapping from char to string(binary)
     */
     HuffmanTree(std::unordered_map<char, std::string> &code);
+
+    /**
+     * @brief Huffman Tree Constructor, build a Huffman tree from a given mapping from char to int
+     * @param byte_cnt :a mapping from char to its frequencies
+    */
     HuffmanTree(std::unordered_map<char, int> &byte_cnt);
-    ~HuffmanTree();
+
+    /**
+     * @brief convert a Huffman tree to a mapping from char to str
+     * @return mapping from char to str decoded from a Huffman tree
+    */
     std::unordered_map<char, std::string> to_huffman_code();
 
 private:
+    /**
+     * @brief recursive build a mapping from char to str from a Hufman tree
+     * @param code :encoded map from char to str
+     * @param node :Huffman tree node used to build a map
+     * @param s :string to store the Huffman code of a char
+    */
     void dfs(std::unordered_map<char, std::string> &code, std::shared_ptr<TreeNode> node, std::string s);
+
+    /**
+     * @brief merge two nodes and let the merged node be their parent
+     * @param node1 :node to be merged
+     * @param node2 :node to be merged
+    */
     std::shared_ptr<TreeNode> join(std::shared_ptr<TreeNode> node1, std::shared_ptr<TreeNode> node2);
 };
 

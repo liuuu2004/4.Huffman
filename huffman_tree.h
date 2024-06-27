1939d359ecf9915188cceb77035939c5d9fd5743
liuuu <liugongmingg@163.com>
2024-06-27 19:45:57 +0800

    build huffman tree

--- huffman_tree.h
+++ huffman_tree.h
@@ -1,5 +1,9 @@
 /**
  * Define a Huffman Tree
+ *
+ * Functions:
+ * 1. if you pass a map<character, frequency> to its constructor, it can encode it to a map<character, string>
+ *    
 */
 
 #ifndef HUFFMAN_TREE_H_
@@ -18,7 +22,7 @@ public:
     std::shared_ptr<TreeNode> root_;
     
     /**
-     * @brief convert a Huffman-encoded string to its original sequence
+     * @brief DECODE :convert a Huffman-encoded string to its original sequence
      * @param s :Huffman-encoded string tobe converted
     */
     std::vector<char> to_byte_array(std::string &s);
@@ -36,14 +40,14 @@ public:
     HuffmanTree(std::unordered_map<char, int> &byte_cnt);
 
     /**
-     * @brief convert a Huffman tree to a mapping from char to str
+     * @brief ENCODE :convert a Huffman tree to a mapping from char to str
      * @return mapping from char to str decoded from a Huffman tree
     */
     std::unordered_map<char, std::string> to_huffman_code();
 
 private:
     /**
-     * @brief recursive build a mapping from char to str from a Hufman tree
+     * @brief ENCODE :recursive build a mapping from char to str from a Hufman tree
      * @param code :encoded map from char to str
      * @param node :Huffman tree node used to build a map
      * @param s :string to store the Huffman code of a char
@@ -51,7 +55,7 @@ private:
     void dfs(std::unordered_map<char, std::string> &code, std::shared_ptr<TreeNode> node, std::string s);
 
     /**
-     * @brief merge two nodes and let the merged node be their parent
+     * @brief ENCODE :merge two nodes and let the merged node be their parent
      * @param node1 :node to be merged
      * @param node2 :node to be merged
     */
